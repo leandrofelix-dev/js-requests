e6d2f33fe09cf530268d2d09587c113c77c1305b
Leandro Félix <58048297+leandrofelix-dev@users.noreply.github.com>
2023-11-13 21:20:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,82 @@
-const buscarGatinhos = (e) => {
-    e.preventDefault()
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=10')
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                const cats = JSON.parse(xhr.responseText)
-                cats.forEach(cat => {
-                    const img = document.createElement('img')
-                    img.src = cat.url
-                    document.querySelector('#gatinhos').appendChild(img)
-                });
-            }
-            else {
-                alert("erro na requisição")
-            }
-        }
-        
-    }
-    xhr.send()
-}
+const urlGatinhos = 'https://api.thecatapi.com/v1/images/search?limit=10';
 
-const btnShow = document.querySelector("#mostrar-gatinhos")
-btnShow.addEventListener("click", buscarGatinhos)
+const buscarGatinhos = (e) => {
+    e.preventDefault();
+    fetch(urlGatinhos, {
+        method: 'get'
+    })
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error(`Ocorreu um erro: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) => {
+        data.forEach(cat => {
+            const img = document.createElement('img');
+            const label = document.createElement('span');
+            const section = document.createElement('section');
+
+            section.className = 'box';
+            section.id = cat.id;
+
+            img.src = cat.url;
+            label.innerHTML = 
+            `<b>Id:</b> ${cat.id},<br>
+             <b>width</b>: ${cat.width}px,<br>
+             <b>height</b>: ${cat.height}px`;
+
+            section.appendChild(img);
+            section.appendChild(label);
+            document.querySelector('#gatinhos').appendChild(section);
+        });
+    })
+    .catch((error) => {
+        console.error('Ocorreu um erro:', error);
+    });
+};
+
+const btnShow = document.querySelector("#mostrar-gatinhos");
+btnShow.addEventListener("click", buscarGatinhos);
+
+
+const urlMarcas = 'https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/data.json';
+
+const buscarMarcas = (e) => {
+    e.preventDefault();
+    fetch(urlMarcas, {
+        method: 'get'
+    })
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error(`Ocorreu um erro: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) => {
+        data.forEach(marca => {
+            const img = document.createElement('img');
+            const label = document.createElement('span');
+            const section = document.createElement('section');
+
+            section.className = 'boxMarcas';
+            section.id = marca.slug;
+
+            img.src = marca.image.optimized;
+            label.innerHTML = 
+            `<b>Name</b>: ${marca.name},<br>
+             <b>Slug</b>: ${marca.slug}px,<br>
+             <b>Link</b>: ${marca.image.source}px`;
+
+            section.appendChild(img);
+            section.appendChild(label);
+            document.querySelector('#marcas').appendChild(section);
+        });
+    })
+    .catch((error) => {
+        console.error('Ocorreu um erro:', error);
+    });
+};
+
+const btnShowCars = document.querySelector("#mostrar-marcas");
+btnShowCars.addEventListener("click", buscarMarcas);
